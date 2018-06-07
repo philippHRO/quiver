f0429d4fe877752415af34661c07433f3be53dd1
philipp <phzacharias@gmail.com>
2018-06-07 10:17:45 +0200

    Update layer_result_generators.py
    Changed "output" to "outputs" and "input" to "inputs".
    Prevent "UserWarning: Update your `Model` call to the Keras 2 API: `Model(inputs=Tensor("co..., outputs=Tensor("ac...)`"

--- quiver_engine/layer_result_generators.py
+++ quiver_engine/layer_result_generators.py
@@ -3,6 +3,6 @@ from keras.models import Model
 
 def get_outputs_generator(model, layer_name):
     return Model(
-        input=model.input,
-        output=model.get_layer(layer_name).output
+        inputs=model.input,
+        outputs=model.get_layer(layer_name).output
     ).predict
